d056641fb65d03c76b1cec364d790d75f32a29cd
Tymon_Solecki <tymon.solecki@gmail.com>
2016-08-23 23:29:27 +0100

    Tymon555

--- index_files/all.js
+++ index_files/all.js
@@ -1,14 +1,25 @@
 /* js for check-boxes*/
   $(document).ready(function(){
     $('.submit').click(function(){
+      // to do - removing old table
+
+      //adding new table
       var x = $('#number').val();
       var content = "<table>"
       for(var i=0; i<x; i++){
-          content += '<tr><td>' + 'result ' +  i + '</td></tr>';
-      }
+        content+='<tr>';
+            for(var j=0; j<x; j++){
+              content += '<td>' +  '</td>';
+              }
+            content+='</tr>';
+            }
       content += "</table>"
 
       $('#here_table').append(content);
 
+    });
+
+    $(document).on('mouseover' , 'td' , function(){
+      $(this).css('background-color' , 'black');
     });
   });
